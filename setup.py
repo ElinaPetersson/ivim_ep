2004a34c2429bcc4aada6b6ad90f65cb09291350
Pelina <77774750+ElinaPetersson@users.noreply.github.com>
2024-07-26 06:06:57 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='ivim',
       version='1.0',
       description='Tools related to Intravoxel Incoherent Motion (IVIM) modeling of diffusion MRI data',
       author='Oscar Jalnefjord',
       author_email='oscar.jalnefjord@gu.se',
-      packages=['ivim'],
+      # packages=['ivim','ivim.preproc','ivim.io','ivim.seq'],
+      packages=find_packages(),
       install_requires = ['numpy', 'scipy', 'nibabel']
      )
